41ecfbad5886e20e71c368e484887b4e02d22b0e
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-02 16:00:32 +0700

    Update

---
+++ System/ActionHandler.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace System;
+
+use AI\AI;
+use Messenger\Messenger;
+
+/**
+ *
+ * @author	Ammar Faizi	<ammarfaizi2@gmail.com>
+ *
+ */
+
+class ActionHandler
+{
+	public function __construct()
+	{
+
+	}
+}
