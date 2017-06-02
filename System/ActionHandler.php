02262d36650762d1407eeb288cf8666234c45b2b
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-02 16:24:57 +0700

    PHP CS Fixer

--- System/ActionHandler.php
+++ System/ActionHandler.php
@@ -16,83 +16,83 @@ use Messenger\Messenger;
 class ActionHandler
 {
 
-	/**
-	 *	
-	 * @var	array
-	 */
-	private $config;
+    /**
+     *
+     * @var	array
+     */
+    private $config;
 
-	/**
-	 *
-	 * @var	AI\AI
-	 */
-	private $ai;
+    /**
+     *
+     * @var	AI\AI
+     */
+    private $ai;
 
-	/**
-	 *
-	 * @var Messenger\Messenger
-	 */
-	private $messenger;
+    /**
+     *
+     * @var Messenger\Messenger
+     */
+    private $messenger;
 
-	/**
-	 *
-	 * @var	array
-	 */
-	private $input;
-
-
-	/**
-	 *
-	 * @param	array	$this->config
-	 */
-	public function __construct($config)
-	{
-		Messenger::setupWebhook(VALIDATION_TOKEN);
-		defined('data') or define('data',__DIR__ . '/data');
-		is_dir(data) or mkdir(data);
-		$this->config		= $this->config;
-		$this->ai			= new AI();
-		$this->messenger	= new Messenger();
-		$this->input		= json_decode(Messenger::get_input(), 1);
-	}
-
-	public function run()
-	{
-		$this->action();
-	}
-
-	private function action()
-	{
-		if (isset($this->input['entry'])) {
-		    foreach ($this->input['entry'] as $key => $value) {
-		        if (isset($this->config[$value['id']]) and isset($value['messaging'])) {
-		            $this->messenger->set_token($this->config[$value['id']]['token']);
-		            print $this->messenger->set_welcome_msg($key, $this->config[$value['id']]['welcome_msg']);
-		            foreach ($value['messaging'] as $value2) {
-		                $to         = $value2['sender']['id'];
-		                $message    = $value2['message']['text'];
-		                $st 		= $this->ai->prepare($message, $this->messenger->get_sender_name($to));
-		                if ($st->execute()) {
-		                	$reply_msg  = $st->fetch_reply();
-		                	if (is_array($reply_msg)) {
-		                		print $this->messenger->send_message($to, $reply_msg[1]);
-		                		print $this->messenger->send_image($to, $reply_msg[0]);
-		                	} else {
-		                		print $this->messenger->send_message($to, $reply_msg);
-		                	}
-		                } else {
-		                	$this->messenger->send_message($to, "Mohon maaf saya belum paham \"{$message}\"");
-		                }
-		            }
-		        }
-		    }
-		}
-	}
+    /**
+     *
+     * @var	array
+     */
+    private $input;
 
 
+    /**
+     *
+     * @param	array	$this->config
+     */
+    public function __construct($config)
+    {
+        Messenger::setupWebhook(VALIDATION_TOKEN);
+        defined('data') or define('data', __DIR__ . '/data');
+        is_dir(data) or mkdir(data);
+        $this->config        = $this->config;
+        $this->ai            = new AI();
+        $this->messenger    = new Messenger();
+        $this->input        = json_decode(Messenger::get_input(), 1);
+    }
+
+    public function run()
+    {
+        $this->action();
+    }
+
+    private function action()
+    {
+        if (isset($this->input['entry'])) {
+            foreach ($this->input['entry'] as $key => $value) {
+                if (isset($this->config[$value['id']]) and isset($value['messaging'])) {
+                    $this->messenger->set_token($this->config[$value['id']]['token']);
+                    print $this->messenger->set_welcome_msg($key, $this->config[$value['id']]['welcome_msg']);
+                    foreach ($value['messaging'] as $value2) {
+                        $to         = $value2['sender']['id'];
+                        $message    = $value2['message']['text'];
+                        $st        = $this->ai->prepare($message, $this->messenger->get_sender_name($to));
+                        if ($st->execute()) {
+                            $reply_msg  = $st->fetch_reply();
+                            if (is_array($reply_msg)) {
+                                print $this->messenger->send_message($to, $reply_msg[1]);
+                                print $this->messenger->send_image($to, $reply_msg[0]);
+                            } else {
+                                print $this->messenger->send_message($to, $reply_msg);
+                            }
+                        } else {
+                            $this->messenger->send_message($to, "Mohon maaf saya belum paham \"{$message}\"");
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 
-	public function __debugInfo()
-	{
-	}
+
+
+    public function __debugInfo()
+    {
+    }
 }
